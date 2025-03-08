cbe85855a711d23c0f41417c7dcaf693c6d33e43
Dimashey <sheykodmitriyjob@gmail.com>
2025-03-08 17:30:21 +0200

    feat(plugins): setup nvim tree keymapping

--- lua/plugins/nvim-tree/init.lua
+++ lua/plugins/nvim-tree/init.lua
@@ -1,12 +1,17 @@
+local map = vim.keymap.set
+
+map("n", "<leader>e", ":NvimTreeToggle<CR>")
+map("n", "<leader>ef", ":NvimTreeFindFile<CR>")
+
 return {
   'nvim-tree/nvim-tree.lua',
   config = function()
     local nvimtree = require "nvim-tree"
 
     nvimtree.setup({
-        view = {
-          width = 60,
-        },
+      view = {
+        width = 60,
+      },
     })
   end,
 }
