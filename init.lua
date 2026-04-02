5e2fc47323ae57fba05d81aca697e84c755744e2
Dimashey <sheykodmitriyjob@gmail.com>
2026-04-02 13:52:30 +0300

    fix(development): remove dap configuration

--- init.lua
+++ init.lua
@@ -1,15 +1,15 @@
---[[ 
+--[[
 Core module is repsponsible for mamanging basic nvim configruration.
 NOTE: This confiration should not be dependent from external sources like plugins.
-]]--
+]] --
 require "core"
---[[ 
+--[[
 Plugins module is repsponsible for mamanging plugins.
 Such as their installation and configuration.
-]]--
+]] --
 require "plugins"
 --[[
 Development module is responsible for nvim configuration for dev proccess.
-For example: LSP and DAP and other dev specific options.
---]]--
+For example: LSP and DAP(in future) and other dev specific options.
+--]] --
 require "development"
--- lua/development/init.lua
+++ lua/development/init.lua
@@ -1,2 +1,1 @@
 require "development.lsp"
-require "development.dap"
