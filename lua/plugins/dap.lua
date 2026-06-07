57026e63ad10e89d2daef3db79b9bc572220d22c
Dimashey <sheykodmitriyjob@gmail.com>
2026-06-07 08:52:29 +0300

    feat: add dap plugin

---
+++ lua/plugins/dap.lua
@@ -0,0 +1,11 @@
+return {
+  "mfussenegger/nvim-dap",
+  dependencies = {
+    "rcarriga/nvim-dap-ui",
+    -- virtual text for the debugger
+    {
+      "theHamsta/nvim-dap-virtual-text",
+      opts = {},
+    },
+  }
+}
